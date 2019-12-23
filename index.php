cb72213420f5473d1f53124bda7b56e508d3ff98
Edson Armando <36713890+SistemaRayoXP@users.noreply.github.com>
2019-12-23 16:58:24 -0600

    Initial update set-up

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es" xml:lang="es">
+	<head>
+		<meta name="msvalidate.01" content="B7EC7548CADCC5ACA79C7A26ADEB57FF" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta name="Description" content="Herramientas, música, artículos de tecnología y más. Solo aqui en Tecnologias Edson Armando"/>
+		<meta name="Robots" content="index, follow"/>
+		<meta name="theme-color" content="#ffffff"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="Tecnologias Edson Armando"/>
+		<meta property="og:description" content="Un sitio web sobre varias cosas."/>
+		<meta property="og:url" content="http://edsonarmando.000webhostapp.com/"/>
+		<meta property="og:image" content="http://edsonarmando.000webhostapp.com/logo.png"/>
+		<title>Inicio - Tecnologias Edson Armando</title>
+		<link rel="canonical" href="http://edsonarmando.000webhostapp.com/"/>
+		<link rel="stylesheet" href="index.css"/>
+		<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"/>
+		<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32"/>
+		<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16"/>
+		<script type="text/javascript" src="mobile.js" defer></script>
+	</head>
+	<body id="body">
+		<?php
+		include 'recursos/1.php'; ?>
+		<div class="subbody">
+			<h1>Bienvenido a Tecnologías Edson Armando.</h1>
+			<p>Esta página es inútil ahora mismo, pero mejorará con el tiempo</p>
+			<?php echo($_SERVER['QUERY_STRING']); ?>
+		</div>
+	</body>
+</html>
